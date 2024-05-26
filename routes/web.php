a3712dffb83c4b58993d279999819fb36febec49
HernandoBun <hernandobun@gmail.com>
2024-05-26 13:30:59 +0700

    Mengubah Blog menajdi posts

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,23 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Hernando Bun', 'title' => 'Contact']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Hernando Bun',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Officia voluptates asperiores quis sequi. Iure ut tempora facere deserunt? 
+            Assumenda eligendi temporibus harum incidunt tempore consectetur saepe minus deserunt dolorum consequatur!'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Hernando Bun',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Officia voluptates asperiores quis sequi. Iure ut tempora facere deserunt? 
+            Assumenda eligendi temporibus harum incidunt tempore consectetur saepe minus deserunt dolorum consequatur!'
+        ]
+    ]]);
 });
 
 Route::get('/contact', function () {
