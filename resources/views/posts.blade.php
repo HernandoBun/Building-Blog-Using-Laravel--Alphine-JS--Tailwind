e02b09eaa34171c34448c3d4b6eedaa2484e45b0
HernandoBun <hernandobun@gmail.com>
2024-05-26 14:28:58 +0700

    Menggunakan Data posts yang sudah dikirim

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,29 +1,16 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
-
+  
+  @foreach ($posts as $post)
   <article class="py-8 max-w-screen-md border-b border-gray-500">
-    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 1</h2>
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{  $post['title'] }}</h2>
     <div> 
-      <a href="#">Hernando Bun</a> | 25 May 2024
+      <a href="#">{{  $post['author'] }}</a> | 25 May 2024
     </div class="textbase text-green-500">
-    <p class="my-4 font-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-       Officia voluptates asperiores quis sequi. Iure ut tempora facere deserunt? 
-       Assumenda eligendi temporibus harum incidunt tempore consectetur saepe minus deserunt dolorum consequatur!
-    </p>
+    <p class="my-4 font-light"> {{ Str::limit($post['body'], 150) }} </p>
     <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
   </article>
-
-  <article class="py-8 max-w-screen-md border-b border-gray-500">
-    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 2</h2>
-    <div> 
-      <a href="#">Hernando Bun</a> | 26 May 2024
-    </div class="textbase text-green-500">
-    <p class="my-4 font-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-       Officia voluptates asperiores quis sequi. Iure ut tempora facere deserunt? 
-       Assumenda eligendi temporibus harum incidunt tempore consectetur saepe minus deserunt dolorum consequatur!
-    </p>
-    <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-  </article>
+  @endforeach
 
 
 
