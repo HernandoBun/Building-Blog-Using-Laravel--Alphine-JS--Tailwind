6fd8559b88f42c4a74bfaff5c20ed918df515f1b
HernandoBun <hernandobun@gmail.com>
2024-05-26 14:57:38 +0700

    Membuat halaman single post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,6 +14,7 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => [
         [
+            'id' => 1,
             'title' => 'Judul Artikel 1',
             'author' => 'Hernando Bun',
             'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -20,6 +22,7 @@ Route::get('/posts', function () {
             Assumenda eligendi temporibus harum incidunt tempore consectetur saepe minus deserunt dolorum consequatur!'
         ],
         [
+            'id' => 2,
             'title' => 'Judul Artikel 2',
             'author' => 'Hernando Bun',
             'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -27,6 +30,33 @@ Route::get('/posts', function () {
             Assumenda eligendi temporibus harum incidunt tempore consectetur saepe minus deserunt dolorum consequatur!'
         ]
     ]]);
+});
+
+Route::get('/posts/{id}', function($id) {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Hernando Bun',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Officia voluptates asperiores quis sequi. Iure ut tempora facere deserunt? 
+            Assumenda eligendi temporibus harum incidunt tempore consectetur saepe minus deserunt dolorum consequatur!'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Hernando Bun',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Officia voluptates asperiores quis sequi. Iure ut tempora facere deserunt? 
+            Assumenda eligendi temporibus harum incidunt tempore consectetur saepe minus deserunt dolorum consequatur!'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($id){
+        return $post['id'] == $id;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
